27e2f4b80ec972c000a94fb35373c64ace4942a7
Sebastian Schmidt <email@sebastian-schmidt.info>
2017-01-08 15:30:39 +0100

    Parsing: Strings, Numbers, Booleans, Tuples.

---
+++ src/parse-key-value.js
@@ -0,0 +1,123 @@
+export function parseKeyValue(output) {
+
+  if (output === null || output === undefined) {
+    return null;
+  }
+
+  let value = output.toString().trim();
+
+  let result = value;
+
+  switch (detectValueType(value)) {
+    case TYPE_STRING:
+      result = parseString(value);
+      break;
+    case TYPE_NUMBER:
+      result = parseNumber(value);
+      break;
+    case TYPE_BOOLEAN:
+      result = parseBoolean(value);
+      break;
+    case TYPE_TUPLE:
+      result = parseTuple(value);
+      break;
+  }
+
+  return result;
+
+}
+
+
+// Detect type:
+
+const TYPE_STRING = Symbol("String");
+const TYPE_NUMBER = Symbol("Number");
+const TYPE_BOOLEAN = Symbol("Boolean");
+const TYPE_TUPLE = Symbol("Tuple");
+const TYPE_UNKNOWN = Symbol("unknown");
+
+function detectValueType(output) {
+
+  let value = output.toString().trim();
+
+  if (isString(value)) {
+    return TYPE_STRING;
+  } else if (isNumber(value)) {
+    return TYPE_NUMBER;
+  } else if (isBoolean(value)) {
+    return TYPE_BOOLEAN;
+  } else if (isTuple(value)) {
+    return TYPE_TUPLE;
+  } else {
+    return TYPE_UNKNOWN;
+  }
+
+}
+
+
+// String:
+
+function isString(value) {
+  return value.startsWith("'") && value.endsWith("'");
+}
+
+function parseString(value) {
+  return value.substring(1, value.length - 1);
+}
+
+
+// Number:
+
+function isNumber(value) {
+  return !isNaN(value);
+}
+
+function parseNumber(value) {
+  return Number(value);
+}
+
+
+// Boolean:
+
+function isBoolean(value) {
+  return value === "true" || value === "false";
+}
+
+function parseBoolean(value) {
+  return value === "true";
+}
+
+
+// Tuple:
+
+function isTuple(value) {
+  return value.startsWith("(") && value.endsWith(")");
+}
+
+function parseTuple(value) {
+
+  const withoutBrackets = value.substring(1, value.length - 1);
+
+  let result = [];
+  let currentValue = null;
+
+  withoutBrackets.split(",").forEach((value) => {
+
+    if (currentValue === null) {
+      currentValue = value;
+    } else {
+      currentValue += "," + value;
+    }
+
+    let type = detectValueType(currentValue);
+
+    if (type !== TYPE_UNKNOWN) {
+      result.push(parseKeyValue(currentValue));
+      currentValue = null;
+    }
+
+  });
+
+  return result;
+
+}
---
+++ tests/parse-key-value.js
@@ -0,0 +1,92 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+
+import { parseKeyValue } from "../src/parse-key-value";
+
+describe("parseKeyValue", () => {
+
+  describe("String", () => {
+
+    it("should return string", () => {
+      expect(parseKeyValue("'Hello World'")).to.be.a("string");
+    });
+
+    it("should return parsed string without surrounding quotes", () => {
+      expect(parseKeyValue("'Hello World'")).to.equal("Hello World");
+    });
+
+  });
+
+  describe("Number", () => {
+
+    it("should return number", () => {
+      expect(parseKeyValue("123")).to.be.a("number");
+    });
+
+    it("should return parsed positive number", () => {
+      expect(parseKeyValue("123")).to.equal(123);
+    });
+
+    it("should return parsed negative number", () => {
+      expect(parseKeyValue("-123")).to.equal(-123);
+    });
+
+  });
+
+  describe("Boolean", () => {
+
+    it("should return boolean", () => {
+      expect(parseKeyValue("true")).to.be.a("boolean");
+    });
+
+    it("should return true", () => {
+      expect(parseKeyValue("true")).to.be.true;
+    });
+
+    it("should return false", () => {
+      expect(parseKeyValue("false")).to.be.false;
+    });
+
+  });
+
+  describe("Tuple", () => {
+
+    it("should return array", () => {
+      expect(parseKeyValue("(123, 456)")).to.be.an.instanceof(Array);
+    });
+
+    it("should return array of numbers", () => {
+      const expected = [123, 456, 789];
+      expect(parseKeyValue("(123, 456, 789)")).to.deep.equal(expected);
+    });
+
+    it("should return array of strings", () => {
+      const expected = ["Hello, ", "world!"];
+      expect(parseKeyValue("('Hello, ', 'world!')")).to.deep.equal(expected);
+    });
+
+    it("should return array of boolean values", () => {
+      const expected = [true, false, true];
+      expect(parseKeyValue("(true, false, true)")).to.deep.equal(expected);
+    });
+
+    it("should return array of arrays of numbers", () => {
+      const expected = [[123, 456], [789, 123]];
+      expect(parseKeyValue("((123, 456), (789, 123))")).to.deep.equal(expected);
+    });
+
+  });
+
+  describe("Other", () => {
+
+    it("should return null if output is null", () => {
+      expect(parseKeyValue(null)).to.be.null;
+    });
+
+    it("should return null if output is undefined", () => {
+      expect(parseKeyValue()).to.be.null;
+    });
+
+  });
+
+});
